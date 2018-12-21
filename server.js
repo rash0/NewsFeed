7174d36fe40a6514d6c6885209b469f156141b32
rash0 <ahmet.rash00@gmail.com>
2018-12-21 11:10:24 +0800

    T.2

---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+// var path = require('path');
+const NewsAPI = require('newsapi');
+// var compression = require('compression')
+const port = process.env.PORT || 5000;
+
+const newsapi = new NewsAPI(process.env.API_KEY);
+
+
+app.get('/lr', function(req, res){
+
+  newsapi.v2.topHeadlines({
+    country: 'us',
+    pageSize: 36
+  }).then(response => {res.send(response)})
+    .catch(err => {
+    console.log('topHeadlines req went wrong  ' + error)
+    res.redirect('/lr')
+  })
+
+});
+
+app.get('/ar/:key/:page/:from/:to/:sort', function(req, res){
+  console.log(req.params.key, req.params.page, req.params.from, req.params.to, req.params.sort)
+  newsapi.v2.everything({
+    q: req.params.key,
+    page: req.params.page,
+    from: req.params.from,
+    to: req.params.to,
+    sortBy: req.params.sort,
+    language: 'en',
+    pageSize: 12,
+  }).then(response => {res.send(response)})
+    .catch(err => {
+    console.log('everything req went wrong  ' + error)
+    res.redirect('/ar')
+  })
+
+
+})
+
+app.listen(port, () =>console.log(`Listening on port ${port}`))
---
+++ src/Components/LiveSidebar/LiveColumn.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import LiveComponent from './LiveComponent'
+import './LiveComponent.css'
+
+
+function LiveColumn(props) {
+  const res = props.response;
+  const listItems = res.map((res, index) =>
+    <LiveComponent
+      key={index}
+      title={res.title}
+      author={res.author}
+      url={res.url}
+      time={res.publishedAt} />
+
+  )
+  return (
+    <div className={ (!props.menustate) ? "bg-dark LiveColumn" : "bg-dark LiveColumn on"}>
+      <div className="row bg-dark pt-2 pl-3">
+        <div className="col-12">
+          <span className="mr-2" />
+          <h5 className="navbar-brand text-white">Headlines</h5>
+        </div>
+      </div>
+      <hr />
+
+      {listItems}
+
+    </div>
+  );
+}
+
+export default LiveColumn;
