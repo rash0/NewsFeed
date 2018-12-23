98daf2b1eab01136edfe70acfda84c88a9afeaf8
rash0 <ahmet.rash00@gmail.com>
2018-12-23 17:41:52 +0800

    T.5

--- src/Components/PostArea/PostComponent.js
+++ src/Components/PostArea/PostComponent.js
@@ -6,7 +6,7 @@ function classItt(index) {
 
   switch (index) {
     case 0:
-      return "col-12 col-md-9 col-xl-6 col-lg-8 pb-3"
+      return "col-12 col-md-9 col-xl-6 col-lg-6 pb-3"
     case 1:
       return "col-6 col-md-3 col-xl-3 col-lg-3"
     case 2:
@@ -16,9 +16,9 @@ function classItt(index) {
     case 4:
       return "col-6 col-md-3 col-xl-3 col-lg-3 order-md-1 pb-3"
     case 5:
-      return "col-12 col-md-9 col-xl-6 col-lg-8 order-xl-1 pb-3"
+      return "col-12 col-md-9 col-xl-6 col-lg-6 order-xl-1 pb-3"
     case 6:
-      return "col-12 col-md-9 col-xl-6 col-lg-8 order-xl-1 pb-3"
+      return "col-12 col-md-9 col-xl-6 col-lg-6 order-xl-1 pb-3"
     case 7:
       return "col-6 col-md-3 col-xl-3 col-lg-3"
     case 8:
@@ -28,7 +28,7 @@ function classItt(index) {
     case 10:
       return "col-6 col-md-3 col-xl-3 col-lg-3 order-md-1 pb-3"
     case 11:
-      return "col-12 col-md-9 col-xl-6 col-lg-8 pb-3"
+      return "col-12 col-md-9 col-xl-6 col-lg-6 pb-3"
     default:
       return null
   }
@@ -41,11 +41,13 @@ function PostComponent(props) {
       <div className={classItt(index)} key={index}>
         <a className="no-deco" href={props.url}>
           <div className="card card-image text-white" style={{backgroundImage: `linear-gradient(to right, rgba(0,0,0,0.79) 0%, rgba(255,255,255,0.18) 100%, rgba(255,255,255,0.17) 100%), url(${props.urlToImage})`,backgroundOrigin: 'border-box', backgroundSize: (classItt(index).slice(0,5)==="col-6")?'150% 100%':'120% 130%', backgroundPosition:(classItt(index).slice(0,5)==="col-6")?'50% 95%':'0' }}>
-          <div>
-            <p className="pl-3 pr-1 pt-3"><small>{(props.author===null || props.author.indexOf("http") !==-1)? props.source.name : props.author}</small></p>
-            <h6 className="pl-3">{props.title}</h6>
+          <div className="card-body">
+            <p className="font-italic text-capitalize"><small>{(props.author===null || props.author.indexOf("http") !==-1)? props.source.name : props.author.toLowerCase()}</small></p>
+            <h6 className="card-title">{props.title.slice(0, 71)}</h6>
           </div>
-          <p className="pl-3 mt-auto "><small>{moment(props.publishedAt).fromNow()}</small></p>
+          <div className="card-link d-flex justify-content-end">
+            <p className="pr-3"><small>{moment(props.publishedAt).format('MMM. DD')}</small></p>
+          </div>
         </div>
         </a>
       </div>
