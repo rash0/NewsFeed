2f7f8313b9c43cdb99f8bb01f187dbeed121fb22
rash0 <ahmet.rash00@gmail.com>
2018-12-24 13:17:23 +0800

    fix linear

---
+++ client/build/precache-manifest.e91fa42aff07643a2c42fd8f2e1b5692.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c9f5af4ec73ac7bb2ebda5f6eb63f6b",
+    "url": "/static/media/set.7c9f5af4.svg"
+  },
+  {
+    "revision": "1f8410366ffbe00f1ce9e163fce2de89",
+    "url": "/static/media/n.1f841036.svg"
+  },
+  {
+    "revision": "0ff4a1b9e36ca7ec0381c37ce55a94f1",
+    "url": "/static/media/N-Logo.0ff4a1b9.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "256c771ea185a8fb3ad8",
+    "url": "/static/js/main.256c771e.chunk.js"
+  },
+  {
+    "revision": "13406eb51d275d8827ac",
+    "url": "/static/js/1.13406eb5.chunk.js"
+  },
+  {
+    "revision": "256c771ea185a8fb3ad8",
+    "url": "/static/css/main.61e32929.chunk.css"
+  },
+  {
+    "revision": "f42b528f4ee08d81f1fb6a5a3eb2cff3",
+    "url": "/index.html"
+  }
+];
